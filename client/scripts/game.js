022cf10fe8e517ee0a7e479797ff2d0506f06512
David Ryan <mail@djryan.eu>
2021-11-11 21:59:21 +0100

    feat: smooth control

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -5,29 +5,31 @@ const debug = document.getElementById('debug-content');
 const mouse = document.getElementById("mouse");
 
 // init canvas
-let lastRender = 0;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d', { alpha: false }); // alpha false for performance reasons (not tested without)
 
 // animation
-let start, previousTimeStamp;
+let lastRender = 0;
+
 
 // window dimensions in global scope
 let viewportWidth; // from browser
 let viewportHeight;
-let viewportX; // top left of viewport,
-let viewportY; // top - usually ship.y - h/2
-let viewportBuffer = 200;
+let viewportX; // top left of viewport
+let viewportY;
+const viewportBuffer = 100;
 
 // size of asteroids
-let asteroidScale = 20;
-let asteroidMaxSize = 5;
+const asteroidScale = 20; // 20
+const asteroidMaxSize = 5;
 
 
 // init field - may come from backend
-const fieldX = 5000;
+const fieldX = 5000; // 5000 x 5000
 const fieldY = 5000;
-const fieldBuffer = 400; // to make sure nothing is spawned too close to edge
+const fieldBuffer = 50; // to make sure nothing is spawned too close to edge
+if (fieldBuffer > 0.5 * Math.min(fieldX, fieldY)) { console.warn("fieldBuffer too large") };
+if (viewportBuffer > 0.5 * Math.min(fieldX, fieldY)) { console.warn("viewportBuffer too large") };
 const asteroids = [];
 const aliens = [];
 const ships = [];
@@ -37,23 +39,27 @@ let myShip = { // x & y are center point
   x: null,
   y: null,
   direction: 0,
-  dX: 0,
-  dY: 0,
+  speed: 0,
   width: 10,
   height: 30,
   maxSpeed: 20,
+  lives: 5,
+  score: 0,
 
-  fire: function () {
-    console.log("GUNS FIRED!!!");
+  shoot: function () {
+    let x = this.x;
+    let y = this.y;
+    let direction = this.direction;
+  shoot(x,y,direction);
   },
 
-  power: function () {
+  thrust: function () {
     // simply translates for testing
     myShip.y = myShip.y - myShip.maxSpeed;
     perimeterCheck();
   },
 
-  back: function () {
+  warp: function () {
     // simply translates for testing
     myShip.y = myShip.y + myShip.maxSpeed;
     perimeterCheck();
@@ -69,17 +75,37 @@ let myShip = { // x & y are center point
     // simply translates for testing
     myShip.x = myShip.x + myShip.maxSpeed;
     perimeterCheck();
+  },
+
+  hit: function () {
+    // if hit, lose a life
   }
+}
+
+const controller = {
+  rotateL: {pressed: false, func: myShip.rotateL},
+  rotateR: {pressed: false, func: myShip.rotateR},
+  thrust: {pressed: false, func: myShip.thrust},
+  warp: {pressed: false, func: myShip.warp},
+  shoot: {pressed: false, func: myShip.shoot}
 }
 
 class Asteroid {
   constructor() {
     this.x = Math.floor(randomX());
     this.y = Math.floor(randomY());
-    this.dx = 0 // Math.round(Math.random()*2-1);
-    this.dy = 0 // Math.round(Math.random()*2-1);
+
+    // need to change to vectore: direction and speed
+    this.dx = (Math.random() * 40) - 20;
+    this.dy = (Math.random() * 40) - 20;
+
     this.size = asteroidMaxSize;
     this.strength = asteroidMaxSize;
+  }
+
+  hit() {
+    // if shot at, decrease strength
+    // if strength = 0, split()
   }
 
   split() {
@@ -97,48 +123,81 @@ window.onload = () => {
 function gameLoop(timestamp) {
   // https://stackoverflow.com/questions/38709923/why-is-requestanimationframe-better-than-setinterval-or-settimeout
   let progress = timestamp - lastRender;
+  positionMyShip();
   updatePositions(progress);
   draw();
 
   lastRender = timestamp;
   // https://dev.to/macroramesh6/are-you-facing-high-cpu-usage-on-animation-requestanimationframe-3c94
   window.requestAnimationFrame(gameLoop)
-
 }
 
 // -----------------------------//
 
 function controls() {
-  document.addEventListener('keydown', (e) => {
-    // will later perhaps absorb the various function into this one, as the ccontrols will not be complicated... Just split for testing
-    // CAPSLOCK??
+  document.addEventListener("keydown", (e) => {
     switch (e.key) {
       case 'W':
       case 'ArrowUp':
-      case 'w': myShip.power(); break;
+      case 'w': controller.thrust.pressed = true; break;
 
       case 'A':
       case 'ArrowLeft':
-        case 'a': myShip.rotateL(); break;
+      case 'a': controller.rotateL.pressed = true; break;
 
       case 'S':
       case 'ArrowDown':
-      case 's': myShip.back(); break;
+      case 's': controller.warp.pressed = true; break;
 
       case 'D':
       case 'ArrowRight':
-      case 'd': myShip.rotateR(); break;
+      case 'd': controller.rotateR.pressed = true; break;
 
-      case ' ': myShip.fire();
+      case ' ': controller.shoot.pressed = true; break;
       default: break;
     }
   });
-  document.addEventListener('mousedown', myShip.fire);
+
+  document.addEventListener("keyup", (e) => {
+    switch (e.key) {
+      case 'W':
+      case 'ArrowUp':
+      case 'w': controller.thrust.pressed = false; break;
+
+      case 'A':
+      case 'ArrowLeft':
+      case 'a': controller.rotateL.pressed = false; break;
+
+      case 'S':
+      case 'ArrowDown':
+      case 's': controller.warp.pressed = false; break;
+
+      case 'D':
+      case 'ArrowRight':
+      case 'd': controller.rotateR.pressed = false; break;
+
+      case ' ': controller.shoot.pressed = false; break;
+      default: break;
+    }
+  });
+
+  document.addEventListener('mousedown', () => { controller.shoot.pressed = false });
+  document.addEventListener('mouseup', () => { controller.shoot.pressed = false });
 
   document.onmousemove = (e) => {
-    console.log("mousin around");
     mouse.innerText = `Mouse x: ${viewportX + e.clientX}, y: ${viewportY + e.clientY}`;
   };
+}
+
+function positionMyShip() {
+// iterate over object
+  //   rotateL: {pressed: false, func: myShip.rotateL},
+
+  Object.values(controller).forEach(property => {
+    if (property.pressed === true) property.func();
+  });
+
+
 }
 
 function perimeterCheck() {
@@ -151,19 +210,21 @@ function perimeterCheck() {
 }
 
 function randomPlacement() {
+
   // asteroids
   for (let x = 0; x < 10; x++) {
     asteroids.push(new Asteroid());
   }
   // myShip
-  myShip.x = 4500 // Math.floor(randomX());
-  myShip.y = 4500 // Math.floor(randomY());
+  myShip.x = Math.floor(randomX());
+  myShip.y = Math.floor(randomY());
 
   // check that ship is not too close to an astroid
   // TODO
 }
 
-function resizeCanvas () { // incase window size changes during play
+function resizeCanvas() { // incase window size changes during play
+
   ctx.strokeStyle = 'black';
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, viewportWidth, viewportHeight);
@@ -175,30 +236,34 @@ function resizeCanvas () { // incase window size changes during play
 }
 
 function updatePositions(progress) {
+
   let fps = 1000 / progress;
   asteroids.forEach((el)=> {
-    el.x = Math.floor(el.x + el.dx / fps);
-    el.y = Math.floor(el.y + el.dy / fps);
+    el.x = el.x + el.dx / fps;
+    el.y = el.y + el.dy / fps;
     if(el.x < -asteroidMaxSize * asteroidScale) el.x = fieldX + asteroidMaxSize * asteroidScale;
   if (el.x > fieldX + asteroidMaxSize * asteroidScale) el.x = - asteroidMaxSize * asteroidScale;
   if (el.y < -asteroidMaxSize * asteroidScale) el.y = fieldY + asteroidMaxSize * asteroidScale;
   if (el.y > fieldY + asteroidMaxSize * asteroidScale) el.y = - asteroidMaxSize * asteroidScale;
-})
+  })
+  perimeterCheck();
 }
 
-function draw () {
+function draw() {
+
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, viewportWidth, viewportHeight);
   viewportX = clamp(myShip.x - viewportWidth / 2, -viewportBuffer, fieldX - viewportWidth + viewportBuffer);
   viewportY = clamp(myShip.y - viewportHeight / 2, -viewportBuffer, fieldY - viewportHeight + viewportBuffer);
-  drawPerimeter();
   drawShip();
   drawAsteroids();
+  drawPerimeter();
   // drawBullets()
   // drawAliens()
 }
 
 function drawPerimeter() {
+
   const border = '#222'
   if (viewportX < 0) {
     ctx.fillStyle = border;
@@ -222,7 +287,8 @@ function drawPerimeter() {
 
 }
 
-function drawAsteroids () {
+function drawAsteroids() {
+
   asteroids.forEach((el) => {
     ctx.beginPath();
     ctx.arc(el.x-viewportX, el.y-viewportY, el.size * asteroidScale, 0, 2 * Math.PI, false);
@@ -235,6 +301,7 @@ function drawAsteroids () {
 }
 
 function drawShip(x, y) {
+
   // plot in center of canvas
   let plotX = (viewportWidth - myShip.width) / 2;
   let plotY = (viewportHeight - myShip.height) / 2;
@@ -256,14 +323,21 @@ function drawShip(x, y) {
   ctx.stroke();
 }
 
-function randomX () {
+function randomX() {
+
   return fieldBuffer + Math.random() * (fieldX - 2 * fieldBuffer);
 }
 
-function randomY () {
+function randomY() {
+
   return fieldBuffer + Math.random() * (fieldY - 2 * fieldBuffer);
 }
 
-function clamp (num, min, max) {
+function clamp(num, min, max) {
+
   return Math.min(Math.max(num, min), max);
+}
+
+function shoot() {
+
 }
