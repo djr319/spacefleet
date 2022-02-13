c3388cd47bbf91ab22af7c99ae754edf0ddb6599
David Ryan <mail@djryan.eu>
2022-02-13 22:31:49 +0100

    fix: correct package.json server script

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ game();
 // ------------ http static ---------- //
 
 app.use(express.static('./views'));
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
